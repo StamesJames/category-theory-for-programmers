881e2e0039d309dfebb521e32c25e21b995dcd87
Benny Smit <benedict.smit@udo.edu>
2023-05-15 15:37:19 +0200

    rust Functor and Bifunctor with GATs solutions

--- rust/src/chap_07/mod.rs
+++ rust/src/chap_07/mod.rs
@@ -1,18 +1,77 @@
-use std::rc::Rc;
+pub trait Functor<'a>
+where Self :'a {
+    type Unpluged : 'a ;
+    type Plug<B:'a>: Functor<'a> + 'a;
 
-fn fmap_reader<'a,A,B,C,G,H>(f: impl Fn(B)->C + 'a) -> impl Fn(G)->H + 'a
-where 
-A: 'a,
-B: 'a,
-C: 'a,
-G: Fn(A) -> B + 'a,
-H: Fn(A) -> C + 'a,
+    fn fmap<F:'a, B:'a>(self, f: F) -> Self::Plug<B>
+    where
+        F: Fn(Self::Unpluged) -> B + 'a;
+}
+
+impl<'a,R:'a,A:'a> Functor<'a> for Box<dyn Fn(R) -> A + 'a> {
+    type Plug<B:'a> = Box<dyn Fn(R)->B + 'a>;
+    type Unpluged = A;
+    fn fmap<F:'a, B:'a>(self, f: F) -> Self::Plug<B>
+        where
+            F: Fn(Self::Unpluged) -> B + 'a {
+        Box::new(move |r| f(self(r)))
+    }
+}
+
+pub fn r_fmap<'a,R:'a,A:'a,B:'a>(g:Box<dyn Fn(R)->A + 'a>,f:Box<dyn Fn(A)->B + 'a>) -> Box<dyn Fn(R)->B + 'a>{
+    Box::new(move |r| f(g(r)))
+}
+
+
+
+
+// Old Implementations 
+/*
+
+trait Functor {
+    type Unpluged;
+    type Plug<B:'static>: Functor;
+
+    fn fmap<F, B>(self, f: F) -> Self::Plug<B>
+    where
+        F: Fn(Self::Unpluged) -> B + 'static;
+}
+
+impl<A> Functor for Option<A>
 {
-    let f = Rc::new(f);
-    return move |g:G|->H {
-        let f = f.clone();
-        return move |a:A|->C {
-            return f(g(a));
+    type Unpluged = A;
+    type Plug<P:'static> = Option<P>;
+    fn fmap<F,B>(self, f: F,) -> Option<B> 
+    where F: Fn(A)->B{
+        match self {
+            None => None,
+            Some(x) => Some(f(x)),
         }
     }
 }
+
+impl<R: 'static,A: 'static> Functor for Box<dyn Fn(R)->A>
+{
+    type Plug<B:'static> = Box<dyn Fn(R)->B>;
+    type Unpluged = A;
+    fn fmap<F, B:'static>(self, f: F) -> Self::Plug<B>
+        where
+            F: Fn(Self::Unpluged) -> B + 'static {
+        Box::new(move |r| f(self(r)))
+    }
+}
+
+impl<A> Functor for Box<A>{
+    type Plug<B:'static> = Box<B>;
+    type Unpluged = A;
+    fn fmap<F, B:'static>(self, f: F) -> Self::Plug<B>
+        where
+            F: Fn(Self::Unpluged) -> B {
+        Box::new(f(*self))
+    }
+}
+
+
+
+
+ */
--- rust/src/chap_08/mod.rs
+++ rust/src/chap_08/mod.rs
@@ -1,5 +1,33 @@
-pub trait Bifunctor<A, B>{
-    fn bimap<NA, NB>(f: impl Fn(A)-> NA, g: impl Fn(B)-> NB, x:impl Bifunctor<A,B>)
-        -> Self 
-        where Self : Sized;
+
+pub trait BiFunctor<'a> 
+where Self : 'a {
+    type Unpluged1: 'a;
+    type Unpluged2: 'a;
+    type Plug<B1:'a,B2:'a>: BiFunctor<'a> + 'a;
+    fn bimap<F1:'a, F2:'a, B1:'a, B2:'a>(self, f1:F1, f2:F2) -> Self::Plug<B1,B2>
+    where
+        F1: Fn(Self::Unpluged1) -> B1 + 'a,
+        F2: Fn(Self::Unpluged2) -> B2 + 'a;
+}
+
+impl<'a,A1:'a,A2:'a> BiFunctor<'a> for (A1,A2) {
+    type Plug<B1:'a,B2:'a> = (B1,B2);
+    type Unpluged1 = A1;
+    type Unpluged2 = A2;
+    fn bimap<F1:'a, F2:'a, B1:'a, B2:'a>(self, f1:F1, f2:F2) -> Self::Plug<B1,B2>
+        where
+            F1: Fn(Self::Unpluged1) -> B1 + 'a,
+            F2: Fn(Self::Unpluged2) -> B2 + 'a {
+        (f1(self.0), f2(self.1))
+    }
+}
+
+pub trait Functor<'a>
+where Self :'a {
+    type Unpluged : 'a ;
+    type Plug<B:'a>: Functor<'a> + 'a;
+
+    fn fmap<F:'a, B:'a>(self, f: F) -> Self::Plug<B>
+    where
+        F: Fn(Self::Unpluged) -> B + 'a;
 }
